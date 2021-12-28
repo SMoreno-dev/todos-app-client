0cc39165cfca577e1a405c726dad7a2cb178cc05
SMoreno-dev <santiago.moreno.2009@gmail.com>
2021-12-28 15:22:15 -0300

    feat: added complete and delete actions on Todo component, included new utils

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,53 +1,32 @@
 import React, { useEffect, useState } from "react";
 import "./Todo.css";
 
+import BearerToken from "../../utils/BearerToken";
+import CheckStatus from "../../utils/CheckStatus";
 import ErrorPanel from "../ErrorPanel/ErrorPanel";
 import { useNavigate } from "react-router";
 
 const Todo = ({ id, title, content, completed }) => {
   //State
-  const [error, setError] = useState({ error: false, message: "" });
-  const [isCompleted, defineIsCompleted] = useState(undefined);
-  const [completedIcon, setCompletedIcon] = useState({
-    class: "",
-    text: "loading...",
+  const [error, setError] = useState({
+    error: false,
+    message: "",
   });
+  const [isCompleted, defineIsCompleted] = useState(undefined);
 
   //Navigate
   const navigate = useNavigate();
 
   //useEffect
   useEffect(() => {
-    const completedIconHandler = () => {
-      switch (isCompleted) {
-        case undefined:
-          return setCompletedIcon({ class: "", text: "Loading..." });
-
-        case true:
-          return setCompletedIcon({ class: "todo-completed", text: "☑" });
-
-        case false:
-          return setCompletedIcon({ class: "todo-completed", text: "❎" });
-
-        default:
-          const errorToThrow = new Error();
-          errorToThrow.message =
-            "isCompleted state should be boolean or undefined";
-          console.error(errorToThrow);
-          break;
-      }
-    };
-    completedIconHandler();
-    defineIsCompleted(completed || false);
-  }, [completed, isCompleted]);
+    defineIsCompleted(completed);
+  }, [completed]);
 
   //PATCH request
   const submitCompleted = async (status) => {
     try {
-      if (!localStorage.token || !localStorage.id) {
-        return navigate("/login");
-      }
-      const bearer = "Bearer " + localStorage.token;
+      const bearer = BearerToken();
+      if (!bearer) return navigate("/login");
 
       const response = await fetch(
         `${process.env.REACT_APP_SERVER_ADDRESS}/todos/${id}`,
@@ -64,54 +43,87 @@ const Todo = ({ id, title, content, completed }) => {
       );
 
       const parsedResponse = await response.json();
-      if (response.status !== 200) {
-        return setError({
-          error: true,
-          message: parsedResponse.message,
-        });
-      }
-
-      window.location.reload();
+      const checkedStatus = CheckStatus(response.status, parsedResponse);
+      return !checkedStatus.error
+        ? window.location.reload()
+        : setError(checkedStatus);
     } catch (error) {
       console.error("ERROR:", error);
       setError({ error: true, message: "Something went wrong." });
     }
   };
 
+  //Delete handler
+  const deleteHandler = async () => {
+    try {
+      const bearer = BearerToken();
+      if (!bearer) return navigate("/login");
+
+      const response = await fetch(
+        `${process.env.REACT_APP_SERVER_ADDRESS}/todos/${id}`,
+        {
+          method: "DELETE",
+          headers: {
+            Authorization: bearer,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      const parsedResponse = await response.json();
+
+      const checkedStatus = CheckStatus(response.status, parsedResponse);
+      return !checkedStatus.error
+        ? window.location.reload()
+        : setError(checkedStatus);
+    } catch (error) {
+      console.error(error);
+      return setError({
+        error: true,
+        message: "Something went wrong deleting this TODO",
+      });
+    }
+  };
+
   //Handler for click action
   const completedHandler = async () => {
     if (isCompleted === false) {
-      submitCompleted(true);
       defineIsCompleted(true);
+      return submitCompleted(true);
     }
 
-    submitCompleted(false);
     defineIsCompleted(false);
+    submitCompleted(false);
   };
-
-  //Handler for completed status icon
 
   return (
     <div className="todo-container">
-      {error.error ? <ErrorPanel message={error.message} /> : null}
-      <div className="todo-box">
-        <div className="todo-title-and-id">
+      {error.error ? (
+        <ErrorPanel
+          message={error.message}
+          redirect={
+            typeof error.redirect !== undefined ? error.redirect : false
+          }
+        />
+      ) : null}
+      <div className="todo-details">
+        <div className="todo-id-and-title">
           <div className="todo-id">{"#" + (id || "0")}</div>
           <div className="todo-title">{title || "Mock Title"}</div>
         </div>
-
-        <br />
-
         <div className="todo-content">
           <p>{content || "Create a TODO collection component"}</p>
         </div>
-
-        <br />
       </div>
-
       <div className="todo-actions">
         <div className="todo-completed" onClick={() => completedHandler()}>
-          <p className={completedIcon.class}>{completedIcon.text}</p>
+          <p className={isCompleted ? "todo-completed" : "todo-not-completed"}>
+            {typeof isCompleted === undefined ? "Loading..." : null}
+            Status: {isCompleted ? "✔" : "✕"}
+          </p>
+        </div>
+        <div className="todo-delete" onClick={() => deleteHandler()}>
+          <p>Delete 🗑</p>
         </div>
       </div>
     </div>
