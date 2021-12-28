7a8e7e2b551c09c717c3b932f8e3608576177cec
SMoreno-dev <santiago.moreno.2009@gmail.com>
2021-12-28 00:55:35 -0300

    feat: added navbar route

--- src/containers/RootContainer.js
+++ src/containers/RootContainer.js
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import LogIn from "../components/LogIn/LogIn";
 import Register from "../components/Register/Register";
+import NavBar from "../components/NavBar/NavBar";
 
 const RootContainer = () => {
   return (
     <div>
       <Router>
+        {<NavBar />}
         <Routes>
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<LogIn />} />
